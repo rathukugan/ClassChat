db287d219bac87c4251b572701e06c288eb20842
Kevin Kim <kevincolour@gmail.com>
2015-11-19 14:03:07 -0500

    lecture backend

--- www/live_lec.php
+++ www/live_lec.php
@@ -1,9 +1,10 @@
 <?php
+    session_start();
     /*connect to database */
     include("sql.php");
     $lec_id = $_GET['id'];
         //find lecture by id from GET
-    $find_lecture = "SELECT class, topic, num, flag FROM lectures WHERE id = '$lec_id'";
+    $find_lecture = "SELECT class, topic, num, flag, voters, satisfied, unsatisfied FROM lectures WHERE id = '$lec_id'";
 
     //get lecture info
     $row = mysql_fetch_array(mysql_query($find_lecture), MYSQL_ASSOC);
@@ -11,6 +12,10 @@
     $course = $row['class'];
     $topic = $row['topic'];
     $flag = $row['flag'];
+    $voters = $row['voters'];
+
+    $satisfied = $row['satisfied'];
+    $unsatisfied = $row['unsatisfied'];
 
     //get course info
     $find_course = "SELECT creator FROM classes WHERE code = '$course'";
@@ -26,19 +31,44 @@
     $row3 = mysql_fetch_array(mysql_query($find_proff), MYSQL_ASSOC);
     $name = $row3['name'];
     
-    /* 'Student question' form uses onsubmit() so it shouldn't affect this one */
-    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    /* Professor ends the lecture session */
+    if (!empty($_POST['end_lecture'])){
         $flag = "ended";
-
-        /*connect to database */
-        include("sql.php");
 
         if ($db_found) {
             /* Update the 'flag' value of the lecture session to 'ended' */
             $SQL = "UPDATE lectures SET flag='$flag' WHERE id = '$lec_id'";
             $result = mysql_query($SQL);
         }
+    }
 
+    /* Students vote in the 'satisfied/unsatisfied' poll */
+    if(!empty($_POST['vote'])){
+        $poll = $_POST['poll'];
+
+        if ($db_found) {
+            /* Update upvote/downvote value in database */
+            if($poll == "satisfied"){
+                $satisfied += 1;
+                $SQL = "UPDATE lectures SET satisfied=satisfied+1 WHERE id = '$lec_id'";
+
+            } else{
+                $unsatisfied += 1;
+                $SQL = "UPDATE lectures SET unsatisfied=unsatisfied+1 WHERE id = '$lec_id'";
+            }
+            $result = mysql_query($SQL);
+
+            /* Update csv of students(by session value) that already voted */
+            if($voters == "empty"){
+                $voters =  $_SESSION['email'];
+            } else{
+                $voters = $voters . ',' . $_SESSION['email'];
+            }
+
+            $SQL = "UPDATE lectures SET voters='$voters' WHERE id ='$lec_id'";
+            $result = mysql_query($SQL);
+            
+        }
     }
     
     
@@ -86,7 +116,7 @@ function post()
                 <form action="" method="post" role="form">
                     <br/><br/><br/>
                     <div class="col-md-7 text-center"> 
-                        <input type="submit" name="submit" id="submit" value="End Lecture Session" class="btn btn-info pull-right">
+                        <input type="submit" name="end_lecture" id="end_lecture" value="End Lecture Session" class="btn btn-info pull-right">
                     </div>
                 </form>
                 
@@ -151,7 +181,38 @@ function post()
                     <div class="col-sm-12">
                         <br/><br/><br/><h1 align="center">The lecture session has ended.</h1>
                     </div>
+
             <?php
+                    $voters_array = explode(",", $voters);
+                    if(in_array($_SESSION['email'], $voters_array)){
+                        /* User has already voted */
+                        ?>
+                        <h3 align="center">Thank you for your vote! Results for lecture satisfaction:</p>
+                        <p align="center">
+                            <span style="font-size: 2.5em; color: green" class="glyphicon glyphicon-thumbs-up"></span>
+                            <span style="margin-right: 20px; font-size: 1.5em"> <?php echo $satisfied;?> </span>
+                            <span style="font-size: 2.5em; color: red" class="glyphicon glyphicon-thumbs-down"></span>
+                            <span style="font-size: 1.5em"> <?php echo $unsatisfied;?> </span>
+                        </h3>
+            <?php
+                    } else{
+                    ?>
+                        <!-- Display a poll to students: satisfied vs. unsatisfied -->
+                        <div class="col-sm-12">
+                            <form action="" method="post" role="form" style="width: 500px; margin: 0 auto; border-style: solid;
+                            padding: 20px">
+                                <h2>How clear was the lecture?</h2>
+                                <div class="radio">
+                                    <label><input type="radio" checked="checked" name="poll" value="satisfied">Satisfied</label>
+                                </div>
+                                <div class="radio">
+                                    <label><input type="radio" name="poll" value="unsatisfied">Unsatisfied</label>
+                                </div>
+                                <input type="submit" name="vote" id="vote" value="Vote" class="btn btn-info">
+                            </form>
+                        </div>
+            <?php
+                    }
                 }
             }
         ?>
--- www/sql1.sql
+++ www/sql1.sql
@@ -51,22 +51,26 @@ CREATE TABLE `lectures` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `num` int(11) NOT NULL,
   `topic` varchar(50) NOT NULL,
-  `flag` varchar(50) NOT NULL
+  `flag` varchar(50) NOT NULL,
+  `satisfied` int(11) NOT NULL,
+  `unsatisfied` int(11) NOT NULL,
+  /* voters is a csv holding the names of students that already voted in the poll */
+  `voters` varchar(50) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lectures`
 --
 
-INSERT INTO `lectures` (`id`, `class`, `date`, `num`, `topic`, `flag`) VALUES
-(1, 'csc301', '2015-10-26 20:39:09', 1, 'swagging', 'ended'),
-(2, 'csc301', '2015-10-26 20:41:54', 2, 'learning to github', 'ended' ),
-(3, 'csc302', '2015-10-26 20:42:04', 1, 'how to java', 'ended'),
-(4, 'csc301', '2015-10-26 20:47:15', 3, 'learning github', 'ended'),
-(5, 'csc301', '2015-10-26 20:56:02', 4, 'learning balls', 'ended'),
-(6, 'csc301', '2015-10-26 20:57:56', 5, 'learning balls 2', 'ended'),
-(7, 'csc309', '2015-10-30 21:01:31', 1, 'github', 'ended'),
-(8, 'csc309', '2015-10-30 21:02:48', 2, 'lol', 'ended');
+INSERT INTO `lectures` (`id`, `class`, `date`, `num`, `topic`, `flag`, `satisfied`, `unsatisfied`, `voters`) VALUES
+(1, 'csc301', '2015-10-26 20:39:09', 1, 'swagging', 'ended', 0, 0, "empty"),
+(2, 'csc301', '2015-10-26 20:41:54', 2, 'learning to github', 'ended', 0, 0, "empty"),
+(3, 'csc302', '2015-10-26 20:42:04', 1, 'how to java', 'ended', 0, 0, "empty"),
+(4, 'csc301', '2015-10-26 20:47:15', 3, 'learning github', 'ended', 0, 0, "empty"),
+(5, 'csc301', '2015-10-26 20:56:02', 4, 'learning balls', 'ended', 0, 0, "empty"),
+(6, 'csc301', '2015-10-26 20:57:56', 5, 'learning balls 2', 'ended', 0, 0, "empty"),
+(7, 'csc309', '2015-10-30 21:01:31', 1, 'github', 'ended', 0, 0, "empty"),
+(8, 'csc309', '2015-10-30 21:02:48', 2, 'lol', 'ended', 0, 0, "empty");
 
 -- --------------------------------------------------------
 
@@ -118,7 +122,8 @@ INSERT INTO `users` (`userid`, `name`, `email`, `password`, `type`) VALUES
 (11, 'Kevin Color', 'kevincolor@gmail.com', '12345', 'Professor'),
 (12, 'Bob', 'bob@utoronto.ca', '12345', 'Professor'),
 (13, 'tom', 'tomorrow@mail.com', '12345', 'Professor'),
-(14, 'rath', 'test@mail.com', '123', 'Student');
+(14, 'rath', 'test@mail.com', '123', 'Student'),
+(15, '1234', '1234@mail.com', '1234', 'Student');
 
 --
 -- Indexes for dumped tables
